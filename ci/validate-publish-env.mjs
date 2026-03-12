991e3d7f730aff009dc1393822d19127bf61edc0
Decept1kon <slash@live.com.au>
2026-03-12 19:26:07 +1000

    feat: github actions and update readme

---
+++ ci/validate-publish-env.mjs
@@ -0,0 +1,23 @@
+import { validateEnv } from "../dist/index.js";
+
+validateEnv(
+  {
+    NODE_AUTH_TOKEN: {
+      type: "string",
+      required: true,
+      validate: (s) =>
+        s.length > 0 ? true : "NODE_AUTH_TOKEN must be set for npm publish",
+    },
+    NODE_ENV: {
+      type: "string",
+      required: false,
+      default: "production",
+    },
+  },
+  {
+    strictCI: true,
+    reporter: "pretty",
+    annotate: true,
+  }
+);
+
